607de66149d1a201d1e8136d944e9a3a89ff1ba3
Teneson125 <kannamanoj2@gmail.com>
2021-07-26 11:26:36 +0530

    creates public and privite routs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch } from 'react-router';
 
 import 'rsuite/dist/styles/rsuite-default.css';
+import PriviteRoute from './components/PriviteRoute';
+import PublicRoute from './components/PublicRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
 
 function App() {
-  return <div>Hey</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PriviteRoute path="/">
+        <Home />
+      </PriviteRoute>
+    </Switch>
+  );
 }
 
 export default App;
