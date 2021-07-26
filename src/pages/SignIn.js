080193142cec3e7920752916def24d0093c586c2
Teneson125 <kannamanoj2@gmail.com>
2021-07-26 22:49:22 +0530

    added signi page with google/facebook

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { Col, Container, Grid, Panel, Row } from 'rsuite';
+import firebase from 'firebase/app';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const Signin = () => {
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profile/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
   return (
     <Container>
-      <Grid>
+      <Grid className="mt-page">
         <Row>
-          <Col xs={24} md={12}>
+          <Col xs={24} md={12} mdOffset={6}>
             <Panel>
-              <div>
+              <div className="text-center">
                 <h2>Welcome to Chat App</h2>
+                <p>Continue with login</p>
+              </div>
+              <div className="mt-4">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Conitnue with Google
+                </Button>
               </div>
             </Panel>
           </Col>
