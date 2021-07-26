bea4e0d221496e7cabcaa1a937e4986c212f808c
Teneson125 <kannamanoj2@gmail.com>
2021-07-26 11:38:35 +0530

    created privite and public route

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Col, Container, Grid, Panel, Row } from 'rsuite';
 
 const Signin = () => {
-  return <div>Sign In</div>;
+  return (
+    <Container>
+      <Grid>
+        <Row>
+          <Col xs={24} md={12}>
+            <Panel>
+              <div>
+                <h2>Welcome to Chat App</h2>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Signin;
