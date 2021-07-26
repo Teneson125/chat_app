525c95e632fc9dbbaf2ee64cc7f7065b7d997bc6
Teneson125 <kannamanoj2@gmail.com>
2021-07-26 10:08:39 +0530

    Initialized Firebase

---
+++ misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCS0EGzuUXx6aVkIyCFEHIJTL4iZF-_MGI',
+  authDomain: 'chat-app-88e9a.firebaseapp.com',
+  projectId: 'chat-app-88e9a',
+  storageBucket: 'chat-app-88e9a.appspot.com',
+  messagingSenderId: '1007792456908',
+  appId: '1:1007792456908:web:c9bf5bd4cdfd7e17966e69',
+};
+
+const app = firebase.initializeApp(config);
